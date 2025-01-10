cc8ad374fdd275b5dfad021607943b55a79ca4aa
Johnny317 <jhonny317espinoza@gmail.com>
2025-01-10 09:27:54 -0500

    Clase VInicio
    si

---
+++ ProyectoFinal/src/proyectofinal/VdeInicio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectofinal;
+
+/**
+ *
+ * @author Jhony Espinoza
+ */
+public class VdeInicio {
+    private String messi;
+    private String goat;
+    
+    
+}
